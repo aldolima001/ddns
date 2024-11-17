4f73974d30868540e80d14f71be40284bba8b115
Aldo Lima (LIMA) <lima@aldolima.com.br>
2024-11-17 18:04:27 -0300

    Update update_ddns_digitalOcen.py
    Aviso: O código presente neste repositório é destinado a fins educacionais e demonstrativos. Ele foi desenvolvido com o objetivo de auxiliar na configuração do DDNS utilizando as plataformas Cloudflare e DigitalOcean. No entanto, o autor não oferece suporte técnico ou garante a sua funcionalidade em todos os cenários.
    
    É fundamental que você:
    
    Revise o código: Analise linha por linha para entender o funcionamento e identificar possíveis vulnerabilidades.
    Adapte o código: Faça as modificações necessárias para que o script se ajuste ao seu ambiente específico.
    Teste o código: Execute o script em um ambiente de testes antes de implementá-lo em produção.
    Consulte a documentação: Consulte a documentação oficial da Cloudflare e DigitalOcean para obter informações mais detalhadas sobre as APIs e as melhores práticas de segurança.
    O autor não se responsabiliza por:
    
    Erros ou omissões: O código pode conter erros ou omissões que podem causar problemas inesperados.
    Danos: O uso indevido deste código pode causar danos ao seu sistema ou a terceiros.
    Violação de segurança: O código pode ser vulnerável a ataques de segurança.

--- update_ddns_digitalOcen.py
+++ update_ddns_digitalOcen.py
@@ -1,3 +1,17 @@
+# Aviso: O código presente neste repositório é destinado a fins educacionais e demonstrativos. Ele foi desenvolvido com o objetivo de auxiliar na configuração do DDNS utilizando as plataformas Cloudflare e DigitalOcean. No entanto, o autor não oferece suporte técnico ou garante a sua funcionalidade em todos os cenários.
+
+# É fundamental que você:
+
+# Revise o código: Analise linha por linha para entender o funcionamento e identificar possíveis vulnerabilidades.
+# Adapte o código: Faça as modificações necessárias para que o script se ajuste ao seu ambiente específico.
+# Teste o código: Execute o script em um ambiente de testes antes de implementá-lo em produção.
+# Consulte a documentação: Consulte a documentação oficial da Cloudflare e DigitalOcean para obter informações mais detalhadas sobre as APIs e as melhores práticas de segurança.
+# O autor não se responsabiliza por:
+
+# Erros ou omissões: O código pode conter erros ou omissões que podem causar problemas inesperados.
+# Danos: O uso indevido deste código pode causar danos ao seu sistema ou a terceiros.
+# Violação de segurança: O código pode ser vulnerável a ataques de segurança.
+
 import requests
 import json
 import click
